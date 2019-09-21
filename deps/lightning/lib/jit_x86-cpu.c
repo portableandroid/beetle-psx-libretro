e61c59855e1e413c030b86977af31994c34f8ad3
Zachary Cook <zachcook1991@gmail.com>
2019-09-20 20:48:54 -0400

    define HAVE_FFSL if not on Windows, since we don't want to redefine it

--- deps/lightning/lib/jit_x86-cpu.c
+++ deps/lightning/lib/jit_x86-cpu.c
@@ -683,7 +683,7 @@ static void _patch_at(jit_state_t*, jit_node_t*, jit_word_t, jit_word_t);
 #    if __X32
 #      define ffsl(i)			ffs(i)
 #    else
-int ffsl(long);
+static int ffsl(long);
 #    endif
 #  endif
 #endif
@@ -3841,7 +3841,7 @@ _patch_at(jit_state_t *_jit, jit_node_t *node,
 }
 
 #  if __X64 && !defined(HAVE_FFSL)
-int
+static int
 ffsl(long i)
 {
     int		bit;
